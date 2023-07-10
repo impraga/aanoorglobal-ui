b50cd7d8288ac164dab8cfaf94eabd5b31df5b4c
Jagadeesh <njagadeesh93@gmail.com>
2023-07-11 04:10:07 +0530

    fix: updated the search logic peer review

--- src/components/atoms/SearchResult/SearchResult.jsx
+++ src/components/atoms/SearchResult/SearchResult.jsx
@@ -1,28 +1,35 @@
-// /* eslint-disable react/prop-types */
 import React from 'react'
-import { PropTypes } from "prop-types";
+import PropTypes from 'prop-types'
 
 import './SearchResult.scss'
 
-const SearchResult = (props) =>
-  <div className='p-2'>{
-    props?.data.map((value) =>
-      <div key={value.title}>
-        <div className='bg-white text-blue br-1 h-100 d-flex flex-row align-items-center justify-content-between p-3 mb-2 bs' >
+const SearchResult = ({ data }) => (
+  <div className="p-2">
+    {data?.map((value, keyName) => (
+      // eslint-disable-next-line react/no-array-index-key
+      <div key={`${value.title}_${keyName}`}>
+        <div className="bg-white text-blue br-1 h-100 d-flex flex-row align-items-center justify-content-between p-3 mb-2 bs">
           <div>
-            <div className='title'>{value.title}</div>
-            <div className='category'>{value.category}</div>
+            <div className="title">{value.title}</div>
+            <div className="category">{value.category}</div>
           </div>
-          <div className='searchresult-btn'>
-            <button type='button' className='text-green'>View more</button>
+          <div className="searchresult-btn">
+            <button type="button" className="text-green">
+              View more
+            </button>
           </div>
         </div>
       </div>
-    )
-  }</div>
+    ))}
+  </div>
+)
 
-// SearchResult.propTypes = {
+SearchResult.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape())
+}
 
-// };
+SearchResult.defaultProps = {
+  data: [{}]
+}
 
 export default SearchResult
--- src/components/molecules/SearchBox/SearchBox.jsx
+++ src/components/molecules/SearchBox/SearchBox.jsx
@@ -1,7 +1,4 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import './SearchBox.scss'
 import search from '../../../../public/assets/icons/search.svg'
 import category from '../../../../public/assets/json/service.json'
@@ -10,26 +7,32 @@ import SearchResult from '../../atoms/SearchResult/SearchResult'
 const SearchBox = () => {
   const [isSearchOpen, setIsSearchOpen] = useState(false)
   const [searchResult, setSearchResult] = useState([])
-  const flatCategory = []
 
   // Maniplating the category data for search results
-  category.forEach((value) => {
-    value.child.forEach((service) => {
-      service.category = value.category
-      flatCategory.push(service)
+  const serviceCategory = useMemo(() => {
+    const flatCategory = []
+    category.forEach((value) => {
+      value.child.forEach((service) => {
+        flatCategory.push({ ...service, category: value.category })
+      })
     })
-  })
+    return flatCategory
+  }, [])
 
   // Function to filter by text
   function searchService(objectArray, quickCriteria) {
-    return objectArray.filter(obj => Object.values(obj).some(val => val ? val.toString().toLowerCase().includes(quickCriteria) : false))
+    return objectArray.filter((obj) =>
+      Object.values(obj).some((val) =>
+        val ? val.toString().toLowerCase().includes(quickCriteria) : false
+      )
+    )
   }
 
   const searchValue = (event) => {
     const { value } = event.target
     if (value) {
       setIsSearchOpen(true)
-      setSearchResult(searchService(flatCategory, value.toLowerCase()))
+      setSearchResult(searchService(serviceCategory, value.toLowerCase()))
     } else {
       setIsSearchOpen(false)
       setSearchResult([])
@@ -38,22 +41,38 @@ const SearchBox = () => {
 
   return (
     <div>
-      <div className={`search-main-cont  ${isSearchOpen ? "active" : ""}`}>
+      <div className={`search-main-cont  ${isSearchOpen ? 'active' : ''}`}>
         <div data-aos="fade-up" data-aos-delay="450">
-          <input onChange={event => searchValue(event)} type="text" className="form-control search-input br-1 bs" id="" placeholder="Search Service" />
-          <button type='submit' className='search-btn bg-green'>
+          <input
+            onChange={(event) => searchValue(event)}
+            type="text"
+            className="form-control search-input br-1 bs"
+            id=""
+            placeholder="Search Service"
+          />
+          <button type="submit" className="search-btn bg-green">
             <img src={search} alt="search button" />
           </button>
         </div>
         <div>
-          <div className={`result-cont bg-white br-1 ${isSearchOpen ? "active" : ""}`}>
+          <div
+            className={`result-cont bg-white br-1 ${
+              isSearchOpen ? 'active' : ''
+            }`}
+          >
             <div>
               <SearchResult data={searchResult} />
             </div>
           </div>
         </div>
       </div>
-      <div className={`blur-bg  ${isSearchOpen ? "active" : ""}`} onClick={() => { setIsSearchOpen(false) }} />
+      <div
+        aria-hidden="true"
+        className={`blur-bg  ${isSearchOpen ? 'active' : ''}`}
+        onClick={() => {
+          setIsSearchOpen(false)
+        }}
+      />
     </div>
   )
 }
