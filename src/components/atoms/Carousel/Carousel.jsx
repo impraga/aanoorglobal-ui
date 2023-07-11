2bb356859fb36c4fd68f4d4865ea7bad63fd993a
Jagadeesh <njagadeesh93@gmail.com>
2023-07-11 22:01:28 +0530

    fix: code review

--- src/components/atoms/Carousel/Carousel.jsx
+++ src/components/atoms/Carousel/Carousel.jsx
@@ -7,21 +7,33 @@ import arrowright from '../../../../public/assets/icons/arrow-blue-right.svg'
 import './Carousel.scss'
 
 const testimonial = [
-  { author: "TCNOM Engineers", city: "Chennai-56", value: "Really we are very much happy about your guidance and support. We always have a good relationship with Aanoor and share / refer with our friends circle about your prompt work all under one roof" },
-  { author: "Brassy Technologies", city: "Chennai-96", value: "Really we are very much happy about your guidance and support. We always have a good relationship with Aanoor and share / refer with our friends circle about your prompt work all under one roof" },
-  { author: "TradeSter wholesale & retail Pvt Ltd ", city: "Chennai-04", value: "Really we are very much happy about your guidance and support. We always have a good relationship with Aanoor and share / refer with our friends circle about your prompt work all under one roof" }
+  {
+    author: 'TCNOM Engineers',
+    city: 'Chennai-56',
+    value:
+      'Really we are very much happy about your guidance and support. We always have a good relationship with Aanoor and share / refer with our friends circle about your prompt work all under one roof'
+  },
+  {
+    author: 'Brassy Technologies',
+    city: 'Chennai-96',
+    value:
+      'Really we are very much happy about your guidance and support. We always have a good relationship with Aanoor and share / refer with our friends circle about your prompt work all under one roof'
+  },
+  {
+    author: 'TradeSter wholesale & retail Pvt Ltd ',
+    city: 'Chennai-04',
+    value:
+      'Really we are very much happy about your guidance and support. We always have a good relationship with Aanoor and share / refer with our friends circle about your prompt work all under one roof'
+  }
 ]
-
-
-
-// Funtion for Swiper
-register()
-
-
 
 const Carousel = () => {
   const swiperElRef = useRef(null)
+
   useEffect(() => {
+    // Funtion for Swiper
+    register()
+
     const swiperContainer = swiperElRef.current
     const params = {
       pagination: true,
@@ -47,32 +59,36 @@ const Carousel = () => {
     swiperContainer.initialize()
   }, [])
 
-  return (<div className='carousel-cont h-100'>
-    <swiper-container ref={swiperElRef}
-      loop="true"
-      speed="500"
-      init="false"
-      navigation="true"
-      pagination="true">
-      {testimonial.map(value =>
-        <swiper-slide key={value.author}>
-          <div className='d-flex flex-column'>
-            <div className='value'>{value.value}</div>
-            <div className='author align-self-end mt-5'>- {value.author}</div>
-            <div className='city align-self-end'>{value.city}</div>
-          </div>
-        </swiper-slide>
-      )}
-    </swiper-container>
-    <div className='btn-cont'>
-      <button type='button' className='bg-green swiper-prev'>
-        <img src={arrowleft} alt="arrow left" />
-      </button>
-      <button type='button' className='bg-green swiper-next'>
-        <img src={arrowright} alt="arrow right" />
-      </button>
+  return (
+    <div className="carousel-cont h-100">
+      <swiper-container
+        ref={swiperElRef}
+        loop="true"
+        speed="500"
+        init="false"
+        navigation="true"
+        pagination="true"
+      >
+        {testimonial.map((value) => (
+          <swiper-slide key={value.author}>
+            <div className="d-flex flex-column">
+              <div className="value">{value.value}</div>
+              <div className="author align-self-end mt-5">- {value.author}</div>
+              <div className="city align-self-end">{value.city}</div>
+            </div>
+          </swiper-slide>
+        ))}
+      </swiper-container>
+      <div className="btn-cont">
+        <button type="button" className="bg-green swiper-prev">
+          <img src={arrowleft} alt="arrow left" />
+        </button>
+        <button type="button" className="bg-green swiper-next">
+          <img src={arrowright} alt="arrow right" />
+        </button>
+      </div>
     </div>
-  </div>)
+  )
 }
 
 export default Carousel
--- src/components/atoms/ExpertButton/ExpertButton.jsx
+++ src/components/atoms/ExpertButton/ExpertButton.jsx
@@ -4,33 +4,35 @@ import './ExpertButton.scss'
 import supportIcon from '../../../../public/assets/icons/support.png'
 import ContactForm from '../../molecules/ContactForm/ContactForm'
 
-
 const ExpertButton = () => {
   const [show, setShow] = useState(false)
 
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
-
-  return <>
-    <button className='expert-button' type='button' onClick={handleShow}>
-      <div className='text'>Connect with expert</div>
-      <div className='circle' />
-      <div className='icon-cont'>
-        <img src={supportIcon} alt="Support Icon" />
-      </div>
-    </button>
-    <Modal
-      show={show}
-      onHide={handleClose}
-      centered
-      dialogClassName="modal-cont"
-    >
-      {/* <Modal.Header closeButton /> */}
-      <Modal.Body className='h-100 position-relative'>
-        <ContactForm />
-      </Modal.Body>
-    </Modal>
-  </>
+  return (
+    <>
+      <button
+        className="expert-button"
+        type="button"
+        onClick={() => setShow(true)}
+      >
+        <div className="text">Connect with expert</div>
+        <div className="circle" />
+        <div className="icon-cont">
+          <img src={supportIcon} alt="Support Icon" />
+        </div>
+      </button>
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        centered
+        dialogClassName="modal-cont"
+      >
+        {/* <Modal.Header closeButton /> */}
+        <Modal.Body className="h-100 position-relative">
+          <ContactForm />
+        </Modal.Body>
+      </Modal>
+    </>
+  )
 }
 
 export default ExpertButton
